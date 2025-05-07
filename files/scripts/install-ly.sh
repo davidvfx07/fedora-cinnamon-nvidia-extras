5c0b2c5c650b8719f32e5a2b84a9ba21b3f04919
David Goldstein <93741283+davidvfx07@users.noreply.github.com>
2025-05-06 20:01:42 -0700

    Update recipe.

---
+++ files/scripts/install-ly.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+cd /var/tmp
+
+#CHANGE WITH ZIG VERSION
+wget https://ziglang.org/download/0.14.0/zig-linux-x86_64-0.14.0.tar.xz -O zig.tar.xz
+tar xf zig.tar.xz
+
+#CHANGE WITH ZIG VERSION
+ZIGPATH=$(pwd)/zig-linux-x86_64-0.14.0
+
+git clone https://codeberg.org/AnErrupTion/ly ly
+cd ly
+
+$ZIGPATH/zig build
+sudo $ZIGPATH/zig build installexe
+
+cd ..
+
+sudo chcon system_u:object_r:xdm_exec_t:s0 /usr/bin/ly
+sudo systemctl enable ly.service
+
+rm -rf $ZIGPATH
+rm -f zig.tar.xz
+rm -rf ly
